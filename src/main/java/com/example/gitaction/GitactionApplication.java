ed3202f17f243d73d556d62b2936030b6e7654bd
joseph <happyjoseph@126.com>
2023-07-10 17:36:50 +0800

    first commit

--- src/main/java/com/example/gitaction/GitactionApplication.java
+++ src/main/java/com/example/gitaction/GitactionApplication.java
@@ -1,10 +1,15 @@
 package com.example.gitaction;
 
+import com.example.gitaction.service.Helloword;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitactionApplication {
+
+	@Autowired
+	Helloword helloword;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitactionApplication.class, args);
--- src/main/java/com/example/gitaction/service/Helloword.java
+++ src/main/java/com/example/gitaction/service/Helloword.java
@@ -1,8 +1,13 @@
 package com.example.gitaction.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Helloword {
     public int calulate(int a, int b)
     {
-        return a *b;
+        int c = a *b;
+        c +=1;
+        return c;
     }
 }
